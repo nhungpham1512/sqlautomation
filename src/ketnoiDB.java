68f2b9ad0b69b5c56c7b2e5c00816a22088ea9c7
phamhongnhung <phamhongnhung1512.phn@gmail.com>
2023-07-20 21:59:38 +0700

    update testcase

--- src/ketnoiDB.java
+++ src/ketnoiDB.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class ketnoiDB {
     private static Connection conn;
@@ -17,12 +14,24 @@ public class ketnoiDB {
 
         try{
             conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName= QLSV ;encrypt=true; trustServerCertificate=true", user, pass);
-            String sql ="INSERT INTO DMSV (MaSV,HoSV,TenSV,Phai,NgaySinh,NoiSinh,MaKhoa,HocBong,MaMH)" + " VALUES('A24','Nguyen','Van An','Nam','05-07-1994','Hà Nội','Vl',Null,'04')";
-            Statement statement = conn.createStatement();
-            int rows = statement.executeUpdate(sql);
-
+//Cach 1:
+//            String sql ="INSERT INTO DMSV (MaSV,HoSV,TenSV,Phai,NgaySinh,NoiSinh,MaKhoa,HocBong,MaMH)" + " VALUES('A25','Nguyen','Khanh Vy','Nam','05-05-1995','Hà Nội','Vl',Null,'04')";
+//            Statement statement = conn.createStatement();
+//            int rows = statement.executeUpdate(sql);
+//Cach 2:
+            String sql ="INSERT INTO DMSV (MaSV,HoSV,TenSV,Phai,NgaySinh,MaMH)" + " VALUES(?,?,?,?,?,?)";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1,"A28");
+            statement.setString(2,"Pham");
+            statement.setString(3,"Nhung");
+            statement.setString(4,"Nu");
+            statement.setString(5,"12-10-1996");
+            statement.setString(6,"04");
+            int rows = statement.executeUpdate();
+            
             if (rows >0) {
                 System.out.println("row has been inserted.");
+                System.out.println("the number of new rows = " + rows);
             }
             conn.close();
         }catch(SQLException e){
